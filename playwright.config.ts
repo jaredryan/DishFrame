99c477f98f6be4ba987bfc0c95c5632d3412c95f
Jared Ryan <jryantennis@gmail.com>
2026-08-08 13:10:18 -0700

    Update playwright and scripts so playwright only tests built servers, since the dev server sometimes gets stuck

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "pnpm run dev",
+    command: process.env.PLAYWRIGHT_WEB_SERVER_COMMAND ?? "pnpm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
